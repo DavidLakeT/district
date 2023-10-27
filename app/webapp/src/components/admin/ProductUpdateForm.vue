aa55a3f569168e1299a2be38ee6f275c1f5f8907
= <57159295+JulianRamirezJ@users.noreply.github.com>
2023-10-27 12:08:53 -0500

    Product Update

--- app/webapp/src/components/admin/ProductUpdateForm.vue
+++ app/webapp/src/components/admin/ProductUpdateForm.vue
@@ -1,38 +1,41 @@
 <template>
-    <div class="product-details">
-      <h2 class="product-title">Update Product: {{ product.name }}</h2>
-  
-      <form @submit.prevent="updateProduct">
-        <form-field :field-value="product.name" label="Name" name="name" :required="true" />
-        <form-field :field-value="product.price" label="Price" name="price" type="number" :required="true" />
-        <form-field :field-value="product.description" label="Description" name="description" type="textarea" :required="true" />
-        <form-field :field-value="product.stock" label="Stock" name="stock" type="number" :required="true" />
-  
-        <button type="submit" class="btn btn-primary">Update Product</button>
-      </form>
-    </div>
-  </template>
-  
-  <script>
-  import FormField from '@/components/common/FormField.vue';
-  
-  export default {
-    props: {
-      product: Object,
+  <div class="product-details">
+    <h2 class="product-title">Update Product: {{ product.name }}</h2>
+
+    <form @submit.prevent="updateProduct">
+      <form-field label="Name:" field-type="text" :field-value="product.name" input-class="form-control" required @update:fieldValue="updatedProduct.name = $event"></form-field>
+      <form-field label="Price:" field-type="number" :field-value="product.price" input-class="form-control" required @update:fieldValue="updatedProduct.price = $event"></form-field>
+      <form-field label="Description:" field-type="textarea" :field-value="product.description" input-class="form-control" required @update:fieldValue="updatedProduct.description = $event"></form-field>
+      <form-field label="Stock:" field-type="number" :field-value="product.stock" input-class="form-control" required @update:fieldValue="updatedProduct.stock = $event"></form-field>
+
+      <button type="submit" class="btn btn-primary">Update Product</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import FormField from '@/components/common/FormField.vue';
+import { updateProduct } from '@/js/products.js';
+
+export default {
+  props: {
+    product: Object,
+  },
+  components: {
+    FormField,
+  },
+  data() {
+    return {
+      updatedProduct: { ...this.product },
+    };
+  },
+  methods: {
+    async updateProduct() {
+      const response = await updateProduct(this.updatedProduct);
+      if (response) {
+        window.location.reload();
+      }
     },
-    components: {
-      FormField,
-    },
-    data() {
-      return {
-        updatedProduct: { ...this.product }, // Make a copy of the prop
-      };
-    },
-    methods: {
-      updateProduct() {
-        // Work with the updatedProduct data to perform the update
-        // ...
-      },
-    },
-  };
-  </script>
+  },
+};
+</script>
--- app/webapp/src/js/products.js
+++ app/webapp/src/js/products.js
@@ -34,7 +34,23 @@ export const fetchProductDetails = async (productId) => {
 
 export const updateProduct = async (productData) => {
   try {
-    const response = await axios.put(`${BASE_URL}/api/product`, productData,{
+    delete productData.reviews;
+    const product_id = productData.id;
+    delete productData.id;
+    const response = await axios.put(`${BASE_URL}/api/product/id/${product_id}`,
+      productData, {
+      withCredentials:true
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error creating product:', error);
+    return [];
+  }
+};
+
+export const deleteProduct = async (productId) => {
+  try {
+    const response = await axios.delete(`${BASE_URL}/api/product/id/${productId}`,{
       withCredentials:true
     });
     return response.data;
