372c2567152ea44866cfe2ddb8ba50d7428a8865
David Cardona <djcardonan@eafit.edu.co>
2023-10-30 12:02:39 -0500

    Implement cart controller and service for the pools

--- app/webapi/controller/cart_controller.go
+++ app/webapi/controller/cart_controller.go
@@ -1,6 +1,11 @@
 package controller
 
-import service "district/service/handler"
+import (
+	service "district/service/handler"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 type CartController struct {
 	servicePool *service.ServicePool
@@ -8,4 +13,31 @@ type CartController struct {
 
 func NewCartController(servicePool *service.ServicePool) *CartController {
 	return &CartController{servicePool: servicePool}
+}
+
+func (cc *CartController) GetCartTotal(c echo.Context) error {
+	_, err := c.Cookie("auth_token")
+	if err != nil {
+		return c.JSON(http.StatusUnauthorized, map[string]interface{}{
+			"error": "you must be logged in to get your cart total",
+		})
+	}
+
+	cartToken, err := c.Cookie("cart_token")
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"error": "could not read your cart information",
+		})
+	}
+
+	cartTotal, err := cc.servicePool.CartService.GetCartTotal(cartToken.Value)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"error": err.Error(),
+		})
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"total": cartTotal,
+	})
 }
