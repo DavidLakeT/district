f0ef0688fd7e1410981adf50f3c5f77e0b71c142
= <57159295+JulianRamirezJ@users.noreply.github.com>
2023-11-07 11:15:57 -0500

    Image loading component implemented

--- app/webapp/src/components/product/ProductCard.vue
+++ app/webapp/src/components/product/ProductCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card">
-    <img :src="imageSrc" class="card-img-top" :alt="product.name">
+    <ImageLoader :productId="product.id" :altText="product.name" class="card-img-top" />
     <div class="card-body">
       <h5 class="card-title">{{ product.name }}</h5>
       <p class="card-text">$ {{ product.price.toFixed(2) }}</p>
@@ -9,28 +9,14 @@
 </template>
 
 <script>
-import { getProductImage } from '@/js/picture.js';
-
+import ImageLoader from '@/components/common/ImageLoader.vue';
 export default {
   name: "ProductCard",
+  components:{
+    ImageLoader
+  },
   props: {
     product: Object,
-  },
-  data() {
-    return {
-      imageSrc: null,
-    };
-  },
-  async created() {
-    try {
-      const response = await getProductImage(this.product.id);
-      if (response) {
-        const blob = new Blob([response], { type: 'image/jpeg' });
-        this.imageSrc = URL.createObjectURL(blob);
-      }
-    } catch (error) {
-      console.error('Error loading product image:', error);
-    }
   },
 };
 </script>
--- app/webapp/src/views/Admin/ProductUpdateView.vue
+++ app/webapp/src/views/Admin/ProductUpdateView.vue
@@ -2,20 +2,20 @@
     <div class="container mt-5">
       <div class="row">
         <div class="col-md-6">
-          <img :src="imageUrl" alt="Product Image" class="img-fluid" />
+          <ImageLoader :productId="this.$route.params.id" class="img-fluid" />
         </div>
         <div class="col-md-6">
           <ProductUpdateForm :product="product" />
         </div>
       </div>
-  
       <product-reviews :reviews="reviews" :product="product" />
     </div>
-  </template>
+</template>
   
   <script>
   import ProductUpdateForm from '@/components/admin/ProductUpdateForm.vue';
   import ProductReviews from '@/components/product/ProductReviews.vue';
+  import ImageLoader from '@/components/common/ImageLoader.vue';
   import { fetchProductDetails } from '@/js/products.js';
   
   export default {
@@ -23,6 +23,7 @@
     components: {
       ProductUpdateForm,
       ProductReviews,
+      ImageLoader,
     },
     data() {
       return {
@@ -30,18 +31,13 @@
         reviews: [],
       };
     },
-    computed: {
-      imageUrl() {
-        return `/images/product${this.$route.params.id}.jpg`;
-      },
-    },
     async created() {
       try {
         const product = await fetchProductDetails(this.$route.params.id);
         this.product = product;
         this.reviews = product.reviews;
       } catch (error) {
-        console.error('Error fetching product data:', error);
+        //console.error('Error fetching product data:', error);
       }
     },
   };
