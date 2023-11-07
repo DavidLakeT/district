363f614b21ab59f9ab08aed5462c4234faafeb2f
= <57159295+JulianRamirezJ@users.noreply.github.com>
2023-11-06 20:11:22 -0500

    Upload image functionality added

---
+++ app/webapp/src/js/picture.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:5000/api/product';
+
+export const getProductImage = async (productNameId) => {
+    try {
+      const response = await axios.get(`${BASE_URL}/picture?path=${productNameId}`, {
+        withCredentials: true,
+        responseType: 'blob', 
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching product image:', error);
+      return null;
+    }
+  };
+  
+
+  export const uploadProductImage = async (imageFile) => {
+    try {
+      const formData = new FormData();
+      formData.append('file', imageFile);
+  
+      const response = await axios.post(`${BASE_URL}/upload`, formData, {
+        withCredentials: true,
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error uploading product image:', error);
+      return null;
+    }
+  };
+  
+  
